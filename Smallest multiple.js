aac1a5fad75ef862bb5025ab11bf8f7be3047f10
İlyas Aktürk <64707279+jhinaryum@users.noreply.github.com>
2020-06-25 23:09:59 +0300

    Add files via upload

---
+++ Smallest multiple.js
@@ -0,0 +1,30 @@
+let notTheNumber = true;
+
+    for(let i = 1; notTheNumber = true; i++){
+
+        if(i/20 %1) continue; {
+            if(i/19 %1) continue; {
+                if(i/18 %1) continue; {
+                    if(i/17 %1) continue; {
+                        if(i/16 %1) continue; {
+                            if(i/15 %1) continue; {
+                                if(i/14 %1) continue; {
+                                    if(i/13 %1) continue; {
+                                        if(i/12 %1) continue; {
+                                            if(i/11 %1) continue; {
+                                                console.log('Number found: ' + i);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+    // burada i değerini 1 baz alıyor ve sonucu bulana kadar da döngü içerisinde sürekli olarak sayılara bölüyor 
